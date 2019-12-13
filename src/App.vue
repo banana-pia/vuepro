15b4f711b343c5c97717f71f86390c69ce05baa8
dazhi <2796358786@qq.com>
2019-12-13 12:51:38 +0800

    git

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class='box'></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+ 
+</script>
+<style lang='stylus' rel="stylesheet/stylus">
+
+  $color = green  
+  boxStyle(w,h,bg)
+    width w
+    height h
+    border none 
+    background bg
+  .box
+    boxStyle(100px,100px,$color)
+
+</style>
